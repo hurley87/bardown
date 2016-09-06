497bbb072d430310281ba589ef87cf1990ec8a8e
david hurley <davidhurley@davids-MacBook-Air.local>
2016-09-06 19:36:15 -0400

    port bug fix web.py

--- web.py
+++ web.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request
+import os
 app = Flask(__name__)
 
 @app.route("/")
@@ -13,4 +14,4 @@ def about():
 
 if __name__ == "__main__":
 	port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+	app.run(host='0.0.0.0', port=port)
