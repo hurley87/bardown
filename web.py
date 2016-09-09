b84544bd2baa5c77adce2f93e7f3b84a199fa411
david hurley <davidhurley@davids-Air.workhaus.bz>
2016-09-09 09:49:22 -0400

    refactor onboarding

--- web.py
+++ web.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 import os
 from find_bars import find_bars
 from get_map import get_map
@@ -17,11 +17,37 @@ GoogleMaps(app, key=os.environ['GOOGLE_MAPS'])
 
 @app.route("/")
 def index():
-	# get user submitted address from URL
-	address = request.values.get('address')
-
 	# generate random code
 	code = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(6))
+
+	return render_template('index.html', code=code)
+
+@app.route("/verify")
+def verify():
+	# get user submitted address from URL
+	number = request.values.get('number')
+	code = request.values.get('code')
+	verify = request.values.get('verify')
+
+	print(number)
+	print(code)
+	print(verify)
+
+	if verify == '':
+		# send code to user in a text
+		verified = send_text(code, "+1{}".format(number))
+		return render_template('verify.html', number=number, code=code, verify=verify)
+	else:
+		print(code == verify)
+		if code == verify:
+			return redirect('/search')
+		else:
+			return render_template('verify.html', number=number, code=code, verify=verify)
+
+
+@app.route("/search")
+def search():
+	address = request.values.get('address')
 
 	# find bars based on address
 	bars = []
@@ -31,23 +57,10 @@ def index():
 	# get map based on bars
 	if bars: 
 		mymap = get_map(bars)
-		return render_template('index.html', bars=bars, mymap=mymap, address=address)
+		return render_template('search.html', bars=bars, mymap=mymap)
 	else:
-		return render_template('index.html', bars=bars, mymap=None, code=code)
-
-@app.route("/verify")
-def verify():
-	# get user submitted address from URL
-	number = request.values.get('number')
-	code = request.values.get('code')
-
-	print(number)
-	print(code)
-	# send code to user in a text
-	verified = send_text(code, "+1{}".format(number))
-
-	return render_template('verify.html')
+		return render_template('search.html', bars=None, mymap=None)
 
 if __name__ == "__main__":
 	port = int(os.environ.get("PORT", 5000))
-	app.run(host='0.0.0.0', port=port, debug=True)
+	app.run(host='0.0.0.0', port=port)
