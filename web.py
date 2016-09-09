aa15d3023121b2e6fe8f9766b334aaa248a58638
david hurley <davidhurley@davids-Air.workhaus.bz>
2016-09-09 10:22:42 -0400

    send user text of locations when they search

--- web.py
+++ web.py
@@ -1,16 +1,17 @@
-from flask import Flask, render_template, request, redirect
-import os
+from flask import Flask, render_template, request, redirect, session
 from find_bars import find_bars
 from get_map import get_map
 from flask_googlemaps import GoogleMaps
-import os
 from dotenv import load_dotenv, find_dotenv
+from send_text import send_text 
+import os
 import random
 import string
-from send_text import send_text 
 load_dotenv(find_dotenv())
 
 app = Flask(__name__)
+
+app.secret_key = os.urandom(24)
 
 # initialize Google maps with API key
 GoogleMaps(app, key=os.environ['GOOGLE_MAPS'])
@@ -26,6 +27,7 @@ def index():
 def verify():
 	# get user submitted address from URL
 	number = request.values.get('number')
+	session['number'] = number
 	code = request.values.get('code')
 	verify = request.values.get('verify')
 
@@ -40,7 +42,7 @@ def verify():
 	else:
 		print(code == verify)
 		if code == verify:
-			return redirect('/search')
+			return redirect('search')
 		else:
 			return render_template('verify.html', number=number, code=code, verify=verify)
 
@@ -48,6 +50,9 @@ def verify():
 @app.route("/search")
 def search():
 	address = request.values.get('address')
+	number = session['number']
+
+	print(number)
 
 	# find bars based on address
 	bars = []
@@ -57,6 +62,15 @@ def search():
 	# get map based on bars
 	if bars: 
 		mymap = get_map(bars)
+
+		for bar in bars:
+			send_text(
+				'{} is located at {}. Contact them at {}'.format(
+					bar['name'], bar['location'], bar['phone']
+				), 
+				"+1{}".format(number)
+			)
+
 		return render_template('search.html', bars=bars, mymap=mymap)
 	else:
 		return render_template('search.html', bars=None, mymap=None)
